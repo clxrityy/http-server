1ed9d8dc746a7af6400fc445a17493ee44f41d49
clxrityy <clxrityxyz@gmail.com>
2025-02-14 16:30:37 -0500

    update maven

--- src/test/java/com/mjanglin/http/HttpHeadersParserTest.java
+++ src/test/java/com/mjanglin/http/HttpHeadersParserTest.java
@@ -51,7 +51,8 @@ public class HttpHeadersParserTest {
         try {
             parseHeadersMethod.invoke(httpParser, generateSpaceBeforeColonError(), request);
         } catch (InvocationTargetException e) {
-            if (e.getCause() instanceof HttpParsingException exception) {
+            if (e.getCause() instanceof HttpParsingException) {
+                HttpParsingException exception = (HttpParsingException) e.getCause();
                 assertEquals(HttpStatusCode.CLIENT_ERROR_400_BAD_REQUEST, exception.getErrorCode());
             }
         }
@@ -67,25 +68,21 @@ public class HttpHeadersParserTest {
     }
 
     private InputStreamReader generateMulitpleHeaderMessage() {
-        String rawData = """
-                         Host: localhost:5006\r
-                         Connection: keep-alive\r
-                         Cache-Control: max-age=0\r
-                         sec-ch-ua: "Chromium";v="130", "Google Chrome";v="130", "Not?A_Brand";v="99"\r
-                         sec-ch-ua-mobile: ?0\r
-                         sec-ch-ua-platform: "macOS"\r
-                         Upgrade-Insecure-Requests: 1\r
-                         User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36\r
-                         Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7\r
-                         Sec-Fetch-Site: none\r
-                         Sec-Fetch-Mode: navigate\r
-                         Sec-Fetch-User: ?1\r
-                         Sec-Fetch-Dest: document\r
-                         Accept-Encoding: gzip, deflate, br, zstd\r
-                         Accept-Language: en-US,en;q=0.9\r
-                         \r
-                         """ //
-                          ;
+        String rawData = "Host: localhost:5006\r\n" +
+                         "Connection: keep-alive\r\n" +
+                         "Cache-Control: max-age=0\r\n" +
+                         "sec-ch-ua: \"Chromium\";v=\"130\", \"Google Chrome\";v=\"130\", \"Not?A_Brand\";v=\"99\"\r\n" +
+                         "sec-ch-ua-mobile: ?0\r\n" +
+                         "sec-ch-ua-platform: \"macOS\"\r\n" +
+                         "Upgrade-Insecure-Requests: 1\r\n" +
+                         "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36\r\n" +
+                         "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7\r\n" +
+                         "Sec-Fetch-Site: none\r\n" +
+                         "Sec-Fetch-Mode: navigate\r\n" +
+                         "Sec-Fetch-User: ?1\r\n" +
+                         "Sec-Fetch-Dest: document\r\n" +
+                         "Accept-Encoding: gzip, deflate, br, zstd\r\n" +
+                         "Accept-Language: en-US,en;q=0.9\r\n\r\n";
         InputStream inputStreamm = new ByteArrayInputStream(rawData.getBytes(StandardCharsets.US_ASCII));
 
         InputStreamReader reader = new InputStreamReader(inputStreamm, StandardCharsets.US_ASCII);
